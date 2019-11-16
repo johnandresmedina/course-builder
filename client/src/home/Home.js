fdeb194561d6449dc1ec80d2b50c3347628d517c
John Medina <jhonandresmedina@gmail.com>
2019-11-16 13:10:48 -0500

    adding jumbotron component

--- client/src/home/Home.js
+++ client/src/home/Home.js
@@ -1,7 +1,27 @@
+import './home.scss';
+
 import React from 'react';
+import { Jumbotron, Button, Row, Col } from 'reactstrap';
 
 const Home = () => {
-  return <h1>Home</h1>;
+  return (
+    <Row>
+      <Col md={{ size: 6, offset: 3 }}>
+        <Jumbotron className='home__jumbotron'>
+          <h1 className='display-3'>Create your course!</h1>
+          <p className='lead'>
+            This is a simple but powerful app that will allow you to create all the courses you need with the right
+            tools.
+          </p>
+          <hr className='my-2' />
+          <p>We care about you and have you cover for this new adventure.</p>
+          <p className='lead'>
+            <Button color='primary'>Create!</Button>
+          </p>
+        </Jumbotron>
+      </Col>
+    </Row>
+  );
 };
 
 export default Home;
