48b1ee53b94fcee120c23232fe961eac792b1e0a
John Medina <jhonandresmedina@gmail.com>
2019-11-16 12:30:41 -0500

    adding basic layout

---
+++ client/src/Layout.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+} from 'reactstrap';
+
+const Layout = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <>
+      <Navbar color='light' light expand='md'>
+        <NavbarBrand href='/'>Course Builder</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className='ml-auto' navbar>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Options
+              </DropdownToggle>
+              <DropdownMenu right>
+                <Link to='/'>
+                  <DropdownItem>Home</DropdownItem>
+                </Link>
+                <Link to='/courses'>
+                  <DropdownItem>Courses</DropdownItem>
+                </Link>
+                <DropdownItem divider />
+                <DropdownItem>Reset</DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+            <NavItem>
+              <NavLink target='_blank' href='https://github.com/johnandresmedina/course-builder'>
+                GitHub
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+      {children}
+    </>
+  );
+};
+
+Layout.propTypes = {
+  children: PropTypes.object,
+};
+
+export default Layout;
